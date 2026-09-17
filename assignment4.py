f0ae0cff8698d2f60374e4e95801d7415809902f
saanvipotdar08-pixel <saanvipotdar08@gmail.com>
2026-09-17 11:41:04 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,23 +1,7 @@
 import numpy as np 
-
-rows = int(input("Enter number of rows: "))
-cols = int(input("Enter number of columns: "))
-
-print("Enter elements of first matrix:")
-elements1 = list(map(int, input().split()))
-matrix1 = np.array(elements1).reshape(rows, cols)
-
-print("Enter elements of second matrix:")
-elements2 = list(map(int, input().split()))
-matrix2 = np.array(elements2).reshape(rows, cols)
-
-result = matrix1 + matrix2
-
-print("Matrix 1:")
-print(matrix1)
-
-print("Matrix 2:")
-print(matrix2)
-
-print("Sum of Matrices:")
-print(result)
+arr1=np.array([[1,2,3],[4,5,6]])
+arr2=np.array([[1,2,3],[4,5,6]])
+result2=arr1+arr2
+print("Matrix 1:\n",arr1)
+print("Matrix 2:\n",arr2)
+print("Addition of m1 and m2:\n",result2)
